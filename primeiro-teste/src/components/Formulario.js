6c18f3858379af0f5c60c0f3be14e9279c6e1dfa
viniciusdossantosandrade <vsa19032009@gmail.com>
2025-09-03 14:44:57 -0300

    Alteraçao do formulario com useState

--- primeiro-teste/src/components/Formulario.js
+++ primeiro-teste/src/components/Formulario.js
@@ -1,16 +1,31 @@
+import {useState} from "react"
 function Formulario(){
 
     function cadastrar(e){
         e.preventDefault()
-        console.log('Cadastrou um usuário')
+        //console.log(nome)
+        //console.log('Cadastrou um usuário')
+        console.log(`Usuario ${nome} cadastro com a senha: ${senha}`)
     }
+
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
     
     return(
         <div>
             <h1>Cadastro</h1>
             <form onSubmit={cadastrar}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"/>
+                    <label htmlfor="nome">Nome:</label>
+                    <input type="text" id="nome" placeholder="Digite seu nome" value={nome}
+                    onChange={(e)=> setNome(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlfor="senha">Senha:</label>
+                    <input type="password" id="senha" placeholder="Digite sua senha"
+                    onChange={(e)=> setSenha(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"/> 
